ac2ac1f5e794c892f925f8493fe7c67fd6053376
Jason Reilly <jdr0887@renci.org>
2013-06-13 14:43:22 -0400

    fixing imports

--- src/main/resources/archetype-resources/__rootArtifactId__-workflow/src/main/java/edu/unc/mapseq/workflow/__mapseqSlashPackageName__/__mapseqPipelineName__Pipeline.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-workflow/src/main/java/edu/unc/mapseq/workflow/__mapseqSlashPackageName__/__mapseqPipelineName__Pipeline.java
@@ -4,8 +4,10 @@
 package edu.unc.mapseq.workflow.${mapseqDotPackageName};
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.List;
 import java.util.ResourceBundle;
-import java.util.UUID;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.jgrapht.DirectedGraph;
@@ -16,11 +18,13 @@ import org.renci.jlrm.condor.CondorJobEdge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.unc.mapseq.module.core.CatCLI;
-import edu.unc.mapseq.module.core.EchoCLI;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.model.HTSFSample;
+import edu.unc.mapseq.dao.model.SequencerRun;
 import edu.unc.mapseq.pipeline.AbstractPipeline;
 import edu.unc.mapseq.pipeline.PipelineException;
 import edu.unc.mapseq.pipeline.PipelineJobFactory;
+import edu.unc.mapseq.pipeline.PipelineUtil;
 
 public class ${mapseqPipelineName}Pipeline extends AbstractPipeline {
 
@@ -50,37 +54,69 @@ public class ${mapseqPipelineName}Pipeline extends AbstractPipeline {
                 CondorJobEdge.class);
 
         int count = 0;
-        String uuid = UUID.randomUUID().toString();
 
-        // new job
-        CondorJob helloJob = PipelineJobFactory.createJob(++count, EchoCLI.class, getWorkflowPlan(), null, false);
-        helloJob.setSiteName("test");
-        helloJob.addArgument(EchoCLI.GREETING, "Hello");
-        File helloJobOutput = new File(getPipelineBeanService().getAttributes().get("outputDirectory"), String.format(
-                "hello-%s.txt", uuid));
-        helloJob.addArgument(EchoCLI.OUTPUT, helloJobOutput.getPath());
-        graph.addVertex(helloJob);
+        if (getWorkflowPlan().getSequencerRun() == null && getWorkflowPlan().getHTSFSamples() == null) {
+            logger.error("Don't have either sequencerRun and htsfSample");
+            throw new PipelineException("Don't have either sequencerRun and htsfSample");
+        }
 
-        // new job
-        CondorJob worldJob = PipelineJobFactory.createJob(++count, EchoCLI.class, getWorkflowPlan(), null, false);
-        worldJob.setSiteName("test");
-        worldJob.addArgument(EchoCLI.GREETING, "World!");
-        File worldJobOutput = new File(getPipelineBeanService().getAttributes().get("outputDirectory"), String.format(
-                "world-%s.txt", uuid));
-        worldJob.addArgument(EchoCLI.OUTPUT, worldJobOutput.getPath());
-        graph.addVertex(worldJob);
+        Set<HTSFSample> htsfSampleSet = new HashSet<HTSFSample>();
 
-        // new job
-        CondorJob catJob = PipelineJobFactory.createJob(++count, CatCLI.class, getWorkflowPlan(), null, false);
-        catJob.setSiteName("test");
-        catJob.addArgument(CatCLI.FILES, helloJobOutput);
-        catJob.addArgument(CatCLI.FILES, worldJobOutput);
-        File catJobOutput = new File(getPipelineBeanService().getAttributes().get("outputDirectory"), String.format(
-                "${parentArtifactId}-%s.txt", uuid));
-        catJob.addArgument(CatCLI.OUTPUT, catJobOutput.getPath());
-        graph.addVertex(catJob);
-        graph.addEdge(helloJob, catJob);
-        graph.addEdge(worldJob, catJob);
+        if (getWorkflowPlan().getSequencerRun() != null) {
+            logger.info("sequencerRun: {}", getWorkflowPlan().getSequencerRun().toString());
+            try {
+                htsfSampleSet.addAll(getPipelineBeanService().getMaPSeqDAOBean().getHTSFSampleDAO()
+                        .findBySequencerRunId(getWorkflowPlan().getSequencerRun().getId()));
+            } catch (MaPSeqDAOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (getWorkflowPlan().getHTSFSamples() != null) {
+            htsfSampleSet.addAll(getWorkflowPlan().getHTSFSamples());
+        }
+
+        for (HTSFSample htsfSample : htsfSampleSet) {
+
+            if ("Undetermined".equals(htsfSample.getBarcode())) {
+                continue;
+            }
+
+            SequencerRun sequencerRun = htsfSample.getSequencerRun();
+            File outputDirectory = createOutputDirectory(sequencerRun.getName(), htsfSample, getName());
+
+            Integer laneIndex = htsfSample.getLaneIndex();
+            logger.debug("laneIndex = {}", laneIndex);
+            List<File> readPairList = PipelineUtil.getReadPairList(htsfSample.getFileDatas(), sequencerRun.getName(),
+                    htsfSample.getLaneIndex());
+            logger.info("fileList = {}", readPairList.size());
+
+            // assumption: a dash is used as a delimiter between a participantId
+            // and the external code
+            int idx = htsfSample.getName().lastIndexOf("-");
+            String participantId = idx != -1 ? htsfSample.getName().substring(0, idx) : htsfSample.getName();
+
+            if (readPairList.size() != 2) {
+                throw new PipelineException("ReadPairList is not 2");
+            }
+
+            File r1FastqFile = readPairList.get(0);
+            String r1FastqRootName = PipelineUtil.getRootFastqName(r1FastqFile.getName());
+
+            File r2FastqFile = readPairList.get(1);
+            String r2FastqRootName = PipelineUtil.getRootFastqName(r2FastqFile.getName());
+
+            String fastqLaneRootName = StringUtils.removeEnd(r2FastqRootName, "_R2");
+
+            try {
+
+                // create graph content here
+
+            } catch (Exception e) {
+                throw new PipelineException(e);
+            }
+
+        }
 
         return graph;
     }
